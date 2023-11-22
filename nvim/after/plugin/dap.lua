4d8e7f4c04b811edb15dfffa6627ba2d76f1074f
davmacario <dav.macario@gmail.com>
2023-11-22 17:05:54 -0600

    Fix shortcuts for debugger

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -1,5 +1,8 @@
 -- Configuration file for nvim-dap (debugger)
-vim.keymap.set("n", "<leader>dt", ":DapUiToggle<CR>", {noremap=true})
+
+require("dapui").setup()
+
+vim.keymap.set("n", "<leader>dt", ":lua require('dapui').toggle()<CR>", {noremap=true})
 vim.keymap.set("n", "<leader>db", ":DapToggleBreakpoint<CR>", {noremap=true})
 vim.keymap.set("n", "<leader>dc", ":DapContinue<CR>", {noremap=true})
 vim.keymap.set("n", "<leader>dr", ":lua require('dapui').open({reset=true})<CR>", {noremap=true})

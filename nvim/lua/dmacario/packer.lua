ff5a879aa2a61d5a8465c7497e3ede96e8a0d4ab
davmacario <dav.macario@gmail.com>
2023-11-27 20:08:39 -0600

    Add chatGPT integration

--- nvim/lua/dmacario/packer.lua
+++ nvim/lua/dmacario/packer.lua
@@ -81,6 +81,18 @@ return require('packer').startup(function(use)
         end,
         ft = { "markdown" },
     })
+    -- OpenAI - GPT integration
+    use({
+        "jackMort/ChatGPT.nvim",
+        config = function()
+            require("chatgpt").setup()
+        end,
+        requires = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim"
+        }
+    })
 
 end)
 

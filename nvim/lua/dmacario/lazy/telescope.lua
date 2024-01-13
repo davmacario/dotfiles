d7cdc67d9b405296384fc29189c29b7d41911c48
davmacario <dav.macario@gmail.com>
2024-01-13 17:02:15 -0600

    Feat: allow telescope 'find_files' to search among gitignored files

--- nvim/lua/dmacario/lazy/telescope.lua
+++ nvim/lua/dmacario/lazy/telescope.lua
@@ -5,7 +5,15 @@ return{
         "nvim-lua/plenary.nvim"
     },
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            pickers = {
+                find_files = {
+                    -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+                    no_ignore = true,
+                },
+            },
+        })
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
@@ -13,5 +21,6 @@ return{
         vim.keymap.set('n', '<leader>fs', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") });
         end)
-    end 
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    end
 }

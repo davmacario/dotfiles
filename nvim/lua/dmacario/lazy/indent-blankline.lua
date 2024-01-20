72b4975d56c6b73d5dbb862a1f0b1188b5305f4c
davmacario <dav.macario@gmail.com>
2024-01-20 17:57:38 -0600

    Fix indent-blankline config

--- nvim/lua/dmacario/lazy/indent-blankline.lua
+++ nvim/lua/dmacario/lazy/indent-blankline.lua
@@ -1,6 +1,21 @@
 return{
     'lukas-reineke/indent-blankline.nvim',
-    config = function()
-        require("ibl").setup()
-    end
+    opts = {
+        exclude = {
+            filetypes = {
+                "help",
+                "alpha",
+                "dashboard",
+                "neo-tree",
+                "Trouble",
+                "trouble",
+                "lazy",
+                "mason",
+                "notify",
+                "toggleterm",
+                "lazyterm",
+            },
+        },
+    },
+    main = "ibl",
 }

68f070bd589d10bc1268b6e69082a4fe97b6d0f4
davmacario <dav.macario@gmail.com>
2024-01-29 00:48:17 -0600

    [fix]: add settings to deactivate netrw (to be reviewed)

--- nvim/lua/dmacario/lazy/nvim-tree.lua
+++ nvim/lua/dmacario/lazy/nvim-tree.lua
@@ -20,6 +20,9 @@ return{
     },
     config = function()
         vim.keymap.set('n', '<leader>o', vim.cmd.NvimTreeToggle)
+        -- Disable netrw:
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
         require("nvim-tree").setup({
             git = {
                 enable = true,

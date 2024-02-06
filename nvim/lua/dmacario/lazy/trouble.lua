3d43eeb3433b6d2b9e9fff0e7bb49b1ecaca4b24
davmacario <dav.macario@gmail.com>
2024-02-06 02:08:57 -0600

    [feat]: fix once and for all icons (errors and autocompletion)

--- nvim/lua/dmacario/lazy/trouble.lua
+++ nvim/lua/dmacario/lazy/trouble.lua
@@ -1,15 +1,27 @@
 return{
-    'folke/trouble.nvim',
-    config = function()
-        local signs = {
-            Error = " ",
-            Warning = " ",
-            Hint = " ",
-            Information = " "
-        }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-        end
-    end
+  'folke/trouble.nvim',
+  opts = {
+    signs = {
+      -- icons / text used for a diagnostic
+      error = "",
+      warning = "",
+      hint = "",
+      information = "",
+      other = "",
+    },
+    use_diagnostic_signs = false,
+  }
+  -- config = function()
+  --   local signs = {
+  --     error = " ",
+  --     warning = " ",
+  --     hint = " ",
+  --     information = " ",
+  --     other = "",
+  --   }
+  --   for type, icon in pairs(signs) do
+  --     local hl = "DiagnosticSign" .. type
+  --     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  --   end
+  -- end
 }

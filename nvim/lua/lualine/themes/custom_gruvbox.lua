235673f06e6b3614a0f5dfe77747e10d53f7641e
davmacario <dav.macario@gmail.com>
2024-02-06 02:08:22 -0600

    [fix]: trying to decide a colorscheme I like

--- nvim/lua/lualine/themes/custom_gruvbox.lua
+++ nvim/lua/lualine/themes/custom_gruvbox.lua
@@ -18,7 +18,7 @@ return {
     c = {bg = colors.darkgray, fg = colors.gray}
   },
   insert = {
-    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
     b = {bg = colors.lightgray, fg = colors.white},
     c = {bg = colors.darkgray, fg = colors.gray}
   },
@@ -33,7 +33,7 @@ return {
     c = {bg = colors.black, fg = colors.white}
   },
   command = {
-    a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
     b = {bg = colors.lightgray, fg = colors.white},
     c = {bg = colors.inactivegray, fg = colors.black}
   },

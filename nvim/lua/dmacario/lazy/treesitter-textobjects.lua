966ae7825b011cc1692ec0e6e11cf8dd7061da20
davmacario <dav.macario@gmail.com>
2024-02-28 16:16:53 -0600

    [fix]: keymaps were messing with me :/

--- nvim/lua/dmacario/lazy/treesitter-textobjects.lua
+++ nvim/lua/dmacario/lazy/treesitter-textobjects.lua
@@ -11,10 +11,10 @@ return {
 
           -- define keymaps: to use them, enter operator ("v", "d", "c", "y") + the keys
           keymaps = {
-            ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
-            ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-            ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
-            ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+            -- ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+            -- ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+            -- ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+            -- ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
           }
         }
       }

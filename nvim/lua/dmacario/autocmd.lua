bdddaaa4a1641a874afab0024c61cf71bb77d0b5
davmacario <dav.macario@gmail.com>
2024-04-11 12:22:19 -0500

    Update markdown settings - wrap lines @ 80

--- nvim/lua/dmacario/autocmd.lua
+++ nvim/lua/dmacario/autocmd.lua
@@ -1,28 +1,33 @@
-local augroup = vim.api.nvim_create_augroup   -- Create/get autocommand group
-local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
+local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
 -- Disable max line length in some files
-augroup('disableLineLength', { clear = true })
-autocmd('Filetype', {
-  group = 'disableLineLength',
-  pattern = { 'text', 'markdown', 'html', 'xhtml', 'typescript', 'json' },
-  command = 'setlocal cc=0 colorcolumn=1000'
+augroup("disableLineLength", { clear = true })
+autocmd("Filetype", {
+  group = "disableLineLength",
+  pattern = { "html", "xhtml", "typescript", "json" },
+  command = "setlocal cc=0 colorcolumn=1000",
+})
+
+augroup("customLineWidth", { clear = true })
+autocmd("Filetype", {
+  group = "customLineWidth",
+  pattern = { "markdown", "latex", "tex" },
+  command = "setlocal colorcolumn=80 textwidth=80",
 })
 
 -- Python custom line lenght (from Black)
-augroup('pythonLineLength', { clear = true})
-autocmd('Filetype', {
-  group = 'pythonLineLength',
-  pattern = { 'python', 'python3', 'py' },
-  command = 'setlocal colorcolumn=88'
+augroup("pythonLineLength", { clear = true })
+autocmd("Filetype", {
+  group = "pythonLineLength",
+  pattern = { "python", "python3", "py" },
+  command = "setlocal colorcolumn=88",
 })
 
 -- Set indentation to 2 spaces for specific filetypes
-augroup('setIndent', { clear = true })
-autocmd('Filetype', {
-  group = 'setIndent',
-  pattern = { 'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript',
-    'yaml', 'lua', 'json'
-  },
-  command = 'setlocal shiftwidth=2 tabstop=2 autoindent'
+augroup("setIndent", { clear = true })
+autocmd("Filetype", {
+  group = "setIndent",
+  pattern = { "xml", "html", "xhtml", "css", "scss", "javascript", "typescript", "yaml", "lua", "json" },
+  command = "setlocal shiftwidth=2 tabstop=2 autoindent",
 })

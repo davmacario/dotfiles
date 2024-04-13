3dde982e269fb618062b2adc3d17f86b4eb8cb40
davmacario <dav.macario@gmail.com>
2024-04-12 21:37:41 -0500

    Successfully configured Zathura + Vimtex

--- nvim/lua/dmacario/lazy/vimtex.lua
+++ nvim/lua/dmacario/lazy/vimtex.lua
@@ -8,8 +8,8 @@ return {
     vim.g.vimtex_view_method = 'zathura'
     vim.g.vimtex_quickfix_mode = 0
     vim.g.vimtex_compiler_latexmk_engines = { ["_"] = "-lualatex" }
-    vim.g.vimtex_view_enabled = 0
-    vim.g.vimtex_view_automatic = 0
+    vim.g.vimtex_view_enabled = 1
+    vim.g.vimtex_view_automatic = 1
     vim.g.vimtex_indent_on_ampersands = 0
   end,
 }

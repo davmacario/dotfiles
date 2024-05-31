91a8f545214174a178590f1cde142ed34b7bc129
davmacario <dav.macario@gmail.com>
2024-05-30 22:09:13 -0500

    feat: add ollama.nvim support

--- nvim/lua/dmacario/lazy/lualine.lua
+++ nvim/lua/dmacario/lazy/lualine.lua
@@ -1,102 +1,124 @@
 local os_icon
 if vim.loop.os_uname().sysname == "Darwin" then
-    os_icon = ""
+	os_icon = ""
 else
-    os_icon = ""
+	os_icon = ""
 end
 
-return{
-  'nvim-lualine/lualine.nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-    'tpope/vim-fugitive',
-  },
-  lazy = false,
-  priority = 1000,
-  config = function()
-    local git_icons = {
-      added    = " ",
-      modified = " ",
-      removed  = " ",
-    }
-    require('lualine').setup {
-      options = {
-        icons_enabled = true,
-        theme = "custom_gruvbox",
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
-        disabled_filetypes = {
-          'NvimTree',
-          'dapui_stacks',
-          'dapui_watches',
-          'dapui_breakpoints',
-          'dapui_scopes',
-          'dapui_console',
-          'dap-repl',
-          statusline = {},
-          winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
-        }
-      },
-      sections = {
-        lualine_a = {{'mode', icon = ""}},
-        lualine_b = {
-          'branch',
-          {
-            "diff",
-            symbols = {
-              added = git_icons.added,
-              modified = git_icons.modified,
-              removed = git_icons.removed,
-            },
-            source = function()
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
-          },
-          {
-            'diagnostics',
-            symbols = {
-              error = "",
-              warn = "",
-              hint = "󰌶",
-              info = "",
-              other = "",
-            }
-          },
-        },
-        lualine_c = {
-          {'filename', path=1},
-          'searchcount'},
-        lualine_x = {'encoding', { "fileformat", symbols = { unix = os_icon } }},
-        lualine_y = {'filetype', 'progress'},
-        lualine_z = {{ 'location', icon = ""}}
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {{'filename', path=2}},
-        lualine_x = {'location'},
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      winbar = {},
-      inactive_winbar = {},
-      extensions = {'fugitive', 'trouble'}
-    }
-  end
+-- Define a function to check that ollama is installed and working
+local function get_condition()
+	return package.loaded["ollama"] and require("ollama").status ~= nil
+end
+
+-- Define a function to check the status and return the corresponding icon
+local function get_status_icon()
+	local status = require("ollama").status()
+
+	if status == "IDLE" then
+		return "󱙺 ~ idle" -- nf-md-robot-outline
+	elseif status == "WORKING" then
+		return "󰚩 ~ busy" -- nf-md-robot
+	end
+end
+
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"tpope/vim-fugitive",
+	},
+	lazy = false,
+	priority = 1000,
+	config = function()
+		local git_icons = {
+			added = " ",
+			modified = " ",
+			removed = " ",
+		}
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "custom_gruvbox",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					"NvimTree",
+					"dapui_stacks",
+					"dapui_watches",
+					"dapui_breakpoints",
+					"dapui_scopes",
+					"dapui_console",
+					"dap-repl",
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				},
+			},
+			sections = {
+				lualine_a = { { "mode", icon = "" } },
+				lualine_b = {
+					"branch",
+					{
+						"diff",
+						symbols = {
+							added = git_icons.added,
+							modified = git_icons.modified,
+							removed = git_icons.removed,
+						},
+						source = function()
+							local gitsigns = vim.b.gitsigns_status_dict
+							if gitsigns then
+								return {
+									added = gitsigns.added,
+									modified = gitsigns.changed,
+									removed = gitsigns.removed,
+								}
+							end
+						end,
+					},
+					{
+						"diagnostics",
+						symbols = {
+							error = "",
+							warn = "",
+							hint = "󰌶",
+							info = "",
+							other = "",
+						},
+					},
+				},
+				lualine_c = {
+					{ "filename", path = 1 },
+					"searchcount",
+				},
+				lualine_x = {
+					get_status_icon,
+					get_condition,
+					"encoding",
+					{ "fileformat", symbols = { unix = os_icon } },
+				},
+				lualine_y = { "filetype", "progress" },
+				lualine_z = { { "location", icon = "" } },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { { "filename", path = 2 } },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = { "fugitive", "trouble" },
+		})
+	end,
 }
---
+++ nvim/lua/dmacario/lazy/ollama-nvim.lua
@@ -0,0 +1,61 @@
+return {
+	"nomnivore/ollama.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"stevearc/dressing.nvim",
+	},
+
+	-- All the user commands added by the plugin
+	cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+
+	keys = {
+		-- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+		{
+			"<leader>OO",
+			":<c-u>lua require('ollama').prompt()<cr>",
+			desc = "ollama prompt",
+			mode = { "n", "v" },
+		},
+
+		-- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+		{
+			"<leader>OG",
+			":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+			desc = "ollama Generate Code",
+			mode = { "n", "v" },
+		},
+	},
+
+	---@type Ollama.Config
+	opts = {
+		model = "llama3",
+		url = "http://100.92.93.57:11434",
+		serve = {
+			command = "docker",
+			args = {
+				"run",
+				"-d",
+				"--rm",
+				"--gpus=all",
+				"-v",
+				"ollama:/root/.ollama",
+				"-p",
+				"11434:11434",
+				"--name",
+				"ollama",
+				"ollama/ollama",
+			},
+			stop_command = "docker",
+			stop_args = { "stop", "ollama" },
+		},
+		-- View the actual default prompts in ./lua/ollama/prompts.lua
+		prompts = {
+			Sample_Prompt = {
+				prompt = "This is a sample prompt that receives $input and $sel(ection), among others.",
+				input_label = "> ",
+				model = "llama3",
+				action = "display",
+			},
+		},
+	},
+}

1f6e63b5d061aae1316afa861856ed950f8f226a
davmacario <dav.macario@gmail.com>
2024-09-27 13:56:00 +0200

    feat: edit telescope ripgrep args

--- nvim/lua/dmacario/lazy/telescope.lua
+++ nvim/lua/dmacario/lazy/telescope.lua
@@ -14,19 +14,28 @@ return {
 						find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
 						no_ignore = true,
 					},
-          live_grep = {
-            find_command = { "rg", "--hidden", "--glob", "!**/.git/*" },
-            no_ignore = true,
-          }
+					live_grep = {
+						additional_args = { "--hidden", "--glob", "!**/.git/*" },
+						no_ignore = true,
+					},
+					grep_string = {
+						find_command = { "--hidden", "--glob", "!**/.git/*" },
+						no_ignore = true,
+					},
 				},
 			})
 
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = 'Telescope find files' })
-			vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = 'Telescope find version-controlled (git) files' })
-			vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = 'Telescope live grep (find text in files)' })
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = 'Telescope find buffers' })
-			vim.keymap.set("n", "<leader>fS", builtin.grep_string, { desc = 'Telescope find string (under cursor)' })
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set(
+				"n",
+				"<leader>fg",
+				builtin.git_files,
+				{ desc = "Telescope find version-controlled (git) files" }
+			)
+			vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Telescope live grep (find text in files)" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope find buffers" })
+			vim.keymap.set("n", "<leader>fS", builtin.grep_string, { desc = "Telescope find string (under cursor)" })
 		end,
 	},
 	{

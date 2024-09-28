25a35f2bf10fcdbf6cf836ddf89d92c7a3739e56
davmacario <dav.macario@gmail.com>
2024-09-28 19:04:17 +0200

    feat: review none-ls setup

--- nvim/lua/dmacario/lazy/none-ls.lua
+++ nvim/lua/dmacario/lazy/none-ls.lua
@@ -10,14 +10,12 @@ return {
 			require("mason-null-ls").setup({
 				ensure_installed = {
 					"stylua",
-					"eslint_d",
 					"prettier",
-					"prettierd",
 					"black",
 					"isort",
+					"flake8",
 					"markdownlint",
 					"clang-format",
-					"shellcheck",
 					"bibtex-tidy",
 					"latexindent",
 				},
@@ -28,14 +26,17 @@ return {
 		"nvimtools/none-ls.nvim",
 		dependencies = {
 			"nvimtools/none-ls-extras.nvim",
+			"gbprod/none-ls-shellcheck.nvim",
 		},
 		config = function()
 			local null_ls = require("null-ls")
 			null_ls.setup({
 				sources = { -- Add here functionalities (formatting, diagnostics, ...)
+					-- Formatting sources
 					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.prettier,
-					null_ls.builtins.formatting.prettierd,
+					null_ls.builtins.formatting.prettier.with({
+						prefer_local = "node_modules/.bin",
+					}),
 					null_ls.builtins.formatting.black,
 					null_ls.builtins.formatting.isort,
 					null_ls.builtins.formatting.markdownlint,
@@ -45,12 +46,20 @@ return {
 						},
 					}),
 
-					require("none-ls.diagnostics.eslint_d"),
+					-- Diagnostics sources
 					null_ls.builtins.diagnostics.markdownlint,
+					require("none-ls.diagnostics.flake8").with({ -- from none-ls-extras
+						extra_args = { "--max-line-length", "88" },
+						prefer_local = ".venv/bin",
+					}),
+					require("none-ls-shellcheck.diagnostics"),
+
+					-- Code actions sources
+					require("none-ls-shellcheck.code_actions"),
 				},
 			})
-			-- Format current buffer with leader + ft
-			vim.keymap.set("n", "<leader>ft", vim.lsp.buf.format, {})
+			-- Keymaps
+			vim.keymap.set("n", "<leader>ft", vim.lsp.buf.format, { desc = "Format current buffer" })
 		end,
 	},
 }

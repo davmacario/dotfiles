8fe2b6a6eee1979081471ceac996bcbcca4163fc
davmacario <dav.macario@gmail.com>
2024-10-07 16:01:04 +0200

    fix: duplicate diagnostic messages
    (cherry picked from commit 0fdefc0f7a7d160415eaeadd158559c5d6e2f44c)

--- nvim/lua/dmacario/lazy/lsp-config.lua
+++ nvim/lua/dmacario/lazy/lsp-config.lua
@@ -86,6 +86,12 @@ return {
 			lspconfig.bashls.setup({
 				capabilities = capabilities,
 				filetypes = { ".sh", "bash", ".bashrc", ".zshrc", ".conf", "sh", "zsh" },
+        settings = {
+          bashIde = {
+            -- Disable shellcheck in bash-language-server (conflicting)
+            shellcheckPath = "",
+          }
+        },
 				on_attach = on_attach,
 			})
 			lspconfig.dockerls.setup({ capabilities = capabilities, on_attach = on_attach })

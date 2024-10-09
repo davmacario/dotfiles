e3ffc8c9b4c870c31c3bcecf18c49de79a3e7f70
davmacario <dav.macario@gmail.com>
2024-10-09 18:33:26 +0200

    style: make merge conflict icon in Nvim-Tree red

--- nvim/lua/dmacario/lazy/nvim-tree.lua
+++ nvim/lua/dmacario/lazy/nvim-tree.lua
@@ -1,61 +1,60 @@
 local function my_on_attach(bufnr)
-  local api = require "nvim-tree.api"
+	local api = require("nvim-tree.api")
 
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
 
-  -- default mappings
-  api.config.mappings.default_on_attach(bufnr)
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
 
-  vim.keymap.set('n', '<C-w>', api.node.open.replace_tree_buffer,     opts('Open: In Place'))
-  vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+	vim.keymap.set("n", "<C-w>", api.node.open.replace_tree_buffer, opts("Open: In Place"))
+	vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
 end
 
-
-return{
-  'nvim-tree/nvim-tree.lua',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
-  config = function()
-    vim.keymap.set('n', '<leader>o', vim.cmd.NvimTreeToggle)
-    -- Disable netrw:
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-    local icons = require("dmacario.style.icons")
-    require("nvim-tree").setup({
-      git = {
-        enable = true,
-      },
-      sort = {
-        sorter = "case_sensitive",
-      },
-      filters = {
-        dotfiles = false,
-        git_ignored = false,
-      },
-      on_attach = my_on_attach,
-      renderer = {
-        icons = icons.tree_icons,
-      },
-    })
-    require("nvim-web-devicons").setup({
-      strict = true,
-      -- Filetype icon override (missing icons)
-      override_by_extension = {
-        toml = {
-          icon = "",
-          color = "#bdb76b",
-          name = "Toml",
-        }
-      }
-    })
-    -- Nvim-tree color overrides
-    vim.cmd("highlight link NvimTreeGitDirtyIcon GruvboxYellow")
-    vim.cmd("highlight link NvimTreeGitStagedIcon GruvboxGreen")
-    vim.cmd("highlight link NvimTreeGitFileNewIcon GruvboxPurple")
-    vim.cmd("highlight link NvimTreeGitNewIcon GruvboxPurple")
-    end,
-  }
-
+return {
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>o", vim.cmd.NvimTreeToggle)
+		-- Disable netrw:
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+		local icons = require("dmacario.style.icons")
+		require("nvim-tree").setup({
+			git = {
+				enable = true,
+			},
+			sort = {
+				sorter = "case_sensitive",
+			},
+			filters = {
+				dotfiles = false,
+				git_ignored = false,
+			},
+			on_attach = my_on_attach,
+			renderer = {
+				icons = icons.tree_icons,
+			},
+		})
+		require("nvim-web-devicons").setup({
+			strict = true,
+			-- Filetype icon override (missing icons)
+			override_by_extension = {
+				toml = {
+					icon = "",
+					color = "#bdb76b",
+					name = "Toml",
+				},
+			},
+		})
+		-- Nvim-tree color overrides
+		vim.cmd("highlight link NvimTreeGitDirtyIcon GruvboxYellow")
+		vim.cmd("highlight link NvimTreeGitStagedIcon GruvboxGreen")
+		vim.cmd("highlight link NvimTreeGitFileNewIcon GruvboxPurple")
+		vim.cmd("highlight link NvimTreeGitNewIcon GruvboxPurple")
+		vim.cmd("highlight link NvimTreeMergeIcon GruvboxRed")
+	end,
+}

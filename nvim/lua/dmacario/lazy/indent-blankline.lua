224a6b0b8330c3bad85a590e394fab1442159dab
davmacario <dav.macario@gmail.com>
2024-10-12 21:09:16 +0200

    style: change indent sign

--- nvim/lua/dmacario/lazy/indent-blankline.lua
+++ nvim/lua/dmacario/lazy/indent-blankline.lua
@@ -1,22 +1,26 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	opts = {
-		exclude = {
-			filetypes = {
-				"help",
-				"alpha",
-				"dashboard",
-				"neo-tree",
-				"Trouble",
-				"trouble",
-				"lazy",
-				"mason",
-				"notify",
-				"toggleterm",
-				"lazyterm",
-			},
-		},
-	},
-	main = "ibl",
-	event = { "BufReadPost", "BufNewFile" },
+  "lukas-reineke/indent-blankline.nvim",
+  opts = {
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+    },
+  },
+  main = "ibl",
+  event = { "BufReadPost", "BufNewFile" },
 }

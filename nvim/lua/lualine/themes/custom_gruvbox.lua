259d27c814a6cc971f3a014203b969a20cf0eb41
davmacario <dav.macario@gmail.com>
2024-10-12 21:10:28 +0200

    style: format

--- nvim/lua/lualine/themes/custom_gruvbox.lua
+++ nvim/lua/lualine/themes/custom_gruvbox.lua
@@ -1,45 +1,49 @@
 local colors = {
-  black        = '#282828',
-  white        = 223,
-  red          = '#fb4934',
-  green        = '#b8bb26',
-  blue         = '#83a598',
-  yellow       = '#fe8019',
-  gray         = '#a89984',
-  darkgray     = '#3c3836',
-  lightgray    = '#504945',
-  inactivegray = '#7c6f64',
+	black = "#282828",
+	white = "#fbf1c7",
+	red = "#fb4934",
+	green = "#b8bb26",
+	blue = "#83a598",
+	yellow = "#fe8019",
+	gray = "#a89984",
+	darkgray = "#3c3836",
+	lightgray = "#504945",
+	inactivegray = "#7c6f64",
+  inactivedarkgray = "#282828"
 }
 
 return {
-  normal = {
-    a = {bg = colors.gray, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.darkgray, fg = colors.gray}
-  },
-  insert = {
-    a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.darkgray, fg = colors.gray}
-  },
-  visual = {
-    a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.black, fg = colors.white}
-  },
-  replace = {
-    a = {bg = colors.red, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.black, fg = colors.white}
-  },
-  command = {
-    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.inactivegray, fg = colors.black}
-  },
-  inactive = {
-    a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
-    b = {bg = colors.darkgray, fg = colors.gray},
-    c = {bg = colors.darkgray, fg = colors.gray}
-  }
+	normal = {
+		a = { bg = colors.gray, fg = colors.black, gui = "bold" },
+		b = { bg = colors.lightgray, fg = colors.white },
+		c = { bg = colors.darkgray, fg = colors.gray },
+	},
+	insert = {
+		a = { bg = colors.blue, fg = colors.black, gui = "bold" },
+		b = { bg = colors.lightgray, fg = colors.white },
+		c = { bg = colors.darkgray, fg = colors.gray },
+	},
+	visual = {
+		a = { bg = colors.yellow, fg = colors.black, gui = "bold" },
+		b = { bg = colors.lightgray, fg = colors.white },
+		c = { bg = colors.black, fg = colors.white },
+	},
+	replace = {
+		a = { bg = colors.red, fg = colors.black, gui = "bold" },
+		b = { bg = colors.lightgray, fg = colors.white },
+		c = { bg = colors.black, fg = colors.white },
+	},
+	command = {
+		a = { bg = colors.green, fg = colors.black, gui = "bold" },
+		b = { bg = colors.lightgray, fg = colors.white },
+		c = { bg = colors.inactivegray, fg = colors.black },
+	},
+	inactive = {
+		a = { bg = colors.inactivedarkgray, fg = colors.gray, gui = "bold" },
+		b = { bg = colors.inactivedarkgray, fg = colors.gray },
+		c = { bg = colors.inactivedarkgray, fg = colors.gray },
+		x = { bg = colors.inactivedarkgray, fg = colors.gray },
+		y = { bg = colors.inactivedarkgray, fg = colors.gray },
+		z = { bg = colors.inactivedarkgray, fg = colors.gray },
+	},
 }

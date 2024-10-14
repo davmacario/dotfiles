842c5f68c452e43a34c431fb1854adbea17294e5
davmacario <dav.macario@gmail.com>
2024-10-14 13:32:11 +0200

    style: fix lualine theme (less changes)
    (cherry picked from commit 92fe85a1cc5144f8e1a8f34ee8d53d4e5b8c4a01)

--- nvim/lua/lualine/themes/custom_gruvbox.lua
+++ nvim/lua/lualine/themes/custom_gruvbox.lua
@@ -9,14 +9,13 @@ local colors = {
 	darkgray = "#3c3836",
 	lightgray = "#504945",
 	inactivegray = "#7c6f64",
-  inactivedarkgray = "#282828"
 }
 
 return {
 	normal = {
 		a = { bg = colors.gray, fg = colors.black, gui = "bold" },
 		b = { bg = colors.lightgray, fg = colors.white },
-		c = { bg = colors.inactivedarkgray, fg = colors.gray },
+		c = { bg = colors.darkgray, fg = colors.gray },
 	},
 	insert = {
 		a = { bg = colors.blue, fg = colors.black, gui = "bold" },
@@ -26,21 +25,21 @@ return {
 	visual = {
 		a = { bg = colors.yellow, fg = colors.black, gui = "bold" },
 		b = { bg = colors.lightgray, fg = colors.white },
-		c = { bg = colors.black, fg = colors.gray },
+		c = { bg = colors.darkgray, fg = colors.gray },
 	},
 	replace = {
 		a = { bg = colors.red, fg = colors.black, gui = "bold" },
 		b = { bg = colors.lightgray, fg = colors.white },
-		c = { bg = colors.black, fg = colors.white },
+		c = { bg = colors.darkgray, fg = colors.white },
 	},
 	command = {
 		a = { bg = colors.green, fg = colors.black, gui = "bold" },
 		b = { bg = colors.lightgray, fg = colors.white },
-		c = { bg = colors.inactivegray, fg = colors.black },
+		c = { bg = colors.darkgray, fg = colors.gray },
 	},
 	inactive = {
-		a = { bg = colors.inactivedarkgray, fg = colors.gray, gui = "bold" },
-		b = { bg = colors.inactivedarkgray, fg = colors.gray },
-		c = { bg = colors.inactivedarkgray, fg = colors.gray },
+		a = { bg = colors.black, fg = colors.gray, gui = "bold" },
+		b = { bg = colors.black, fg = colors.gray },
+		c = { bg = colors.black, fg = colors.gray },
 	},
 }

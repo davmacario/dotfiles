f9eeb0a6c652532e87d97f05c44ec7d9be1e67b1
Davide Macario <dav.macario@gmail.com>
2024-10-16 22:32:52 +0200

    style: formatting

--- nvim/lua/dmacario/autocmd.lua
+++ nvim/lua/dmacario/autocmd.lua
@@ -4,99 +4,99 @@ local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 -- Disable max line length in some files
 augroup("disableLineLength", { clear = true })
 autocmd("Filetype", {
-	group = "disableLineLength",
-	pattern = { "html", "xhtml", "typescript", "json", "markdown" },
-	command = "setlocal cc=0 colorcolumn=1000",
+  group = "disableLineLength",
+  pattern = { "html", "xhtml", "typescript", "json", "markdown" },
+  command = "setlocal cc=0 colorcolumn=1000",
 })
 
 -- LaTeX settings
 augroup("md_latex_settings", { clear = true })
 autocmd("Filetype", {
-	group = "md_latex_settings",
-	pattern = { "latex", "tex" },
-	command = "setlocal textwidth=80 conceallevel=2",
+  group = "md_latex_settings",
+  pattern = { "latex", "tex" },
+  command = "setlocal textwidth=80 conceallevel=2",
 })
 
 -- For python files, grab the maximum line length from teh active flake8 config
 -- and use it to set the value of colorcolumn
 local function get_flake8_max_line_length()
-	local config_files = { ".flake8", "setup.cfg", "tox.ini" }
-	for _, filename in ipairs(config_files) do
-		local file = io.open(filename, "r")
-		if file then
-			for line in file:lines() do
-				local max_line_length = line:match("^%s*max%-line%-length%s*=%s*(%d+)")
-				if max_line_length then
-					file:close()
-					return tonumber(max_line_length)
-				end
-			end
-			file:close()
-		end
-	end
-	-- Default line length if no config found (same as in ~/.flake8)
-	return 88
+  local config_files = { ".flake8", "setup.cfg", "tox.ini" }
+  for _, filename in ipairs(config_files) do
+    local file = io.open(filename, "r")
+    if file then
+      for line in file:lines() do
+        local max_line_length = line:match("^%s*max%-line%-length%s*=%s*(%d+)")
+        if max_line_length then
+          file:close()
+          return tonumber(max_line_length)
+        end
+      end
+      file:close()
+    end
+  end
+  -- Default line length if no config found (same as in ~/.flake8)
+  return 88
 end
 
 -- Python custom line lenght
 augroup("pythonLineLength", { clear = true })
 autocmd("Filetype", {
-	group = "pythonLineLength",
-	pattern = { "python", "python3", "py" },
-	callback = function()
-		local max_line_length = get_flake8_max_line_length()
-		if max_line_length then
-			vim.wo.colorcolumn = tostring(max_line_length)
-		end
-	end,
+  group = "pythonLineLength",
+  pattern = { "python", "python3", "py" },
+  callback = function()
+    local max_line_length = get_flake8_max_line_length()
+    if max_line_length then
+      vim.wo.colorcolumn = tostring(max_line_length)
+    end
+  end,
 })
 
 -- Set indentation to 2 spaces for specific filetypes
 augroup("setIndent", { clear = true })
 autocmd("Filetype", {
-	group = "setIndent",
-	pattern = {
-		"xml",
-		"html",
-		"xhtml",
-		"css",
-		"scss",
-		"javascript",
-		"typescript",
-		"yaml",
-		"yml",
-		"lua",
-		"json",
-		"markdown",
-		"c",
-		"cpp",
-		"cc",
-		".h",
-		".hpp",
-	},
-	command = "setlocal expandtab shiftwidth=2 tabstop=2",
+  group = "setIndent",
+  pattern = {
+    "xml",
+    "html",
+    "xhtml",
+    "css",
+    "scss",
+    "javascript",
+    "typescript",
+    "yaml",
+    "yml",
+    "lua",
+    "json",
+    "markdown",
+    "c",
+    "cpp",
+    "cc",
+    ".h",
+    ".hpp",
+  },
+  command = "setlocal expandtab shiftwidth=2 tabstop=2",
 })
 
 -- Treat Jenkinsfile as groovy
 augroup("jenkinsGroovy", { clear = true })
 autocmd({ "Filetype", "BufRead", "BufNewFile" }, {
-	group = "jenkinsGroovy",
-	pattern = { ".jenkins", "Jenkinsfile", "jenkinsfile", "jenkins" },
-	command = "set filetype=groovy",
+  group = "jenkinsGroovy",
+  pattern = { ".jenkins", "Jenkinsfile", "jenkinsfile", "jenkins" },
+  command = "set filetype=groovy",
 })
 
 -- Set correct filetype for gitconfig (enable treesitter)
 augroup("gitconfig", { clear = true })
 autocmd({ "Filetype", "BufRead", "BufNewFile" }, {
-	group = "gitconfig",
-	pattern = { "*.gitconfig", ".gitconfig.*" },
-	command = "set filetype=gitconfig",
+  group = "gitconfig",
+  pattern = { "*.gitconfig", ".gitconfig.*" },
+  command = "set filetype=gitconfig",
 })
 
 -- Always remove trailing whitespaces
 autocmd("BufWritePre", {
-	pattern = { "*" },
-	callback = function()
-		vim.cmd([[%s/\s\+$//e]])
-	end,
+  pattern = { "*" },
+  callback = function()
+    vim.cmd([[%s/\s\+$//e]])
+  end,
 })
--- nvim/lua/dmacario/lazy_init.lua
+++ nvim/lua/dmacario/lazy_init.lua
@@ -1,26 +1,26 @@
 -- Lazy.nvim setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	spec = "dmacario.lazy",
-	change_detection = { notify = false },
+  spec = "dmacario.lazy",
+  change_detection = { notify = false },
   -- ui config
-	ui = {
-		border = "rounded",
-		size = {
-			width = 0.8,
-			height = 0.8,
-		},
-	},
+  ui = {
+    border = "rounded",
+    size = {
+      width = 0.8,
+      height = 0.8,
+    },
+  },
 })

c4db76aac2df1e08acb8281ade64d7450949b2a9
Davide Macario <dav.macario@gmail.com>
2024-11-16 16:04:08 +0100

    feat: extra gitsigns settings
    - New remap (use Gitsigns blame)
    - Set current line blame on
    
    (cherry picked from commit 07c5da38f99a53986079da8ea76838683e4fd128)

--- nvim/lua/dmacario/lazy/gitsigns.lua
+++ nvim/lua/dmacario/lazy/gitsigns.lua
@@ -7,6 +7,8 @@ return {
 		local icons = require("dmacario.style.icons")
 		require("gitsigns").setup({
 			signs = icons.gitsigns,
+			current_line_blame = true,
 		})
+		vim.keymap.set("n", "<leader>gb", ":Gitsigns blame", { desc = "Toggle git blame window" })
 	end,
 }

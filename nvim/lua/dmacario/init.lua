bcc1f08908a0710cf66e4928ef9f38d80d848cdf
Davide Macario <dav.macario@gmail.com>
2024-11-17 13:05:17 +0100

    chore: import user commands
    (cherry picked from commit 6d15dedeb1152a149b93a60585ddeb3c37b9f37b)

--- nvim/lua/dmacario/init.lua
+++ nvim/lua/dmacario/init.lua
@@ -4,3 +4,4 @@ require("dmacario.remap") -- Key mappings
 require("dmacario.set") -- General Vim settings
 require("dmacario.autocmd") -- Autocmd definitions
 require("dmacario.lazy_init") -- Lazy Init. file
+require("dmacario.user_commands") -- User commands

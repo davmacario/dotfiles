a78d0d1d55330561016cc1f10285219a168d12c2
Davide Macario <dav.macario@gmail.com>
2024-11-18 00:49:00 +0100

    fix: pin version

--- nvim/lua/dmacario/lazy/visual-whitespace.lua
+++ nvim/lua/dmacario/lazy/visual-whitespace.lua
@@ -1,5 +1,6 @@
 return {
 	"mcauley-penney/visual-whitespace.nvim",
+	commit = "f52e8302095d947407ac28cd6f41b7b0bb1d4fc8",
 	config = function()
 		require("visual-whitespace").setup({
 			highlight = { fg = "#a89984", bg = "#665c54" },

73a3b42a8a37f6afa2bf7dd0cdc963eb047fc3f2
Davide Macario <dav.macario@gmail.com>
2024-11-18 15:41:28 +0100

    fix: add <cr> to enter command

--- nvim/lua/dmacario/lazy/fugitive.lua
+++ nvim/lua/dmacario/lazy/fugitive.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
     -- No vim.cmd because I want to see the command written in the Vim command line
-		vim.keymap.set("n", "<leader>gps", ":Git push", { noremap = true })
-		vim.keymap.set("n", "<leader>gpu", ":Git pull -p", { noremap = true })
+		vim.keymap.set("n", "<leader>gps", ":Git push<CR>", { noremap = true })
+		vim.keymap.set("n", "<leader>gpu", ":Git pull -p<CR>", { noremap = true })
 	end,
 }

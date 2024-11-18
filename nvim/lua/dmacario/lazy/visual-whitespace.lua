6548e2a2238f0f9fe2df8b16bada7b82fbfa9e5c
Davide Macario <dav.macario@gmail.com>
2024-11-18 08:23:48 +0100

    fix: update visual whitespace config
    Unpin version - bug was fixed

--- nvim/lua/dmacario/lazy/visual-whitespace.lua
+++ nvim/lua/dmacario/lazy/visual-whitespace.lua
@@ -1,14 +1,13 @@
 return {
-	"mcauley-penney/visual-whitespace.nvim",
-	commit = "f52e8302095d947407ac28cd6f41b7b0bb1d4fc8",
-	config = function()
-		require("visual-whitespace").setup({
-			highlight = { fg = "#a89984", bg = "#665c54" },
-			space_char = "·",
-			tab_char = "→",
-			nl_char = "↲",
-			cr_char = "←",
-			enabled = true,
-		})
-	end,
+  "mcauley-penney/visual-whitespace.nvim",
+  config = function()
+    require("visual-whitespace").setup({
+      highlight = { fg = "#a89984", bg = "#665c54" },
+      space_char = "·",
+      tab_char = "→",
+      nl_char = "↲",
+      cr_char = "←",
+      enabled = true,
+    })
+  end,
 }

940624380e9c3fcbd91d8e22b039f4b7f8eda119
Davide Macario <dav.macario@gmail.com>
2024-11-18 15:40:46 +0100

    fix: remove vim.cmd to allow to display command
    (cherry picked from commit d866455dda34d63f06e514101a6540b2d2d032d7)

--- nvim/lua/dmacario/lazy/fugitive.lua
+++ nvim/lua/dmacario/lazy/fugitive.lua
@@ -2,11 +2,8 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-		vim.keymap.set("n", "<leader>gps", function()
-			vim.cmd("Git push")
-		end, { noremap = true })
-		vim.keymap.set("n", "<leader>gpu", function()
-			vim.cmd("Git pull -p")
-		end, { noremap = true })
+    -- No vim.cmd because I want to see the command written in the Vim command line
+		vim.keymap.set("n", "<leader>gps", ":Git push", { noremap = true })
+		vim.keymap.set("n", "<leader>gpu", ":Git pull -p", { noremap = true })
 	end,
 }

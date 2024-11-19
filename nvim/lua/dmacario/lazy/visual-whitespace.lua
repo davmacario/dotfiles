69757e276425166f21bed7c52c728bc0a3812a50
Davide Macario <dav.macario@gmail.com>
2024-11-19 09:36:06 +0100

    style: change color of visual whitespace

--- nvim/lua/dmacario/lazy/visual-whitespace.lua
+++ nvim/lua/dmacario/lazy/visual-whitespace.lua
@@ -2,7 +2,7 @@ return {
   "mcauley-penney/visual-whitespace.nvim",
   config = function()
     require("visual-whitespace").setup({
-      highlight = { fg = "#a89984", bg = "#665c54" },
+      highlight = { fg = "#908070", bg = "#665c54" },
       space_char = "·",
       tab_char = "→",
       nl_char = "↲",

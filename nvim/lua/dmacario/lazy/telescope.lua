a54adfd1d6f03cec2abec7890c575322b087496d
Davide Macario <dav.macario@gmail.com>
2024-11-21 09:12:26 +0100

    fix: show hidden file in Telescope file explorer

--- nvim/lua/dmacario/lazy/telescope.lua
+++ nvim/lua/dmacario/lazy/telescope.lua
@@ -80,6 +80,10 @@ return {
 						},
 					},
 					file_browser = {
+            hidden = {
+              file_browser = true,
+              folder_browser = true,
+            },
 						theme = "ivy",
 						-- disables netrw and use telescope-file-browser in its place
 						-- hijack_netrw = true,

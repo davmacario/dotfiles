6414e0dc0b8fb9a7f1553aba1badbed5fc3ad574
Davide Macario <dav.macario@gmail.com>
2025-02-06 09:10:32 +0100

    feat: set up codecompanion for local development

--- nvim/lua/dmacario/lazy/codecompanion.lua
+++ nvim/lua/dmacario/lazy/codecompanion.lua
@@ -48,42 +48,80 @@ return {
 					},
 				},
 			},
-			system_prompt = function()
-				return [[
-				You are an AI programming assistant named "CodeCompanion". You are currently plugged in to text editor on a user's machine.
+			-- system_prompt = function()
+			-- 	return [[
+			-- 	You are an AI programming assistant named "CodeCompanion". You are currently plugged in to text editor on a user's machine.
 
-				Your core tasks include:
-				- Answering general programming questions.
-				- Explaining how the code in a Neovim buffer works.
-				- Reviewing the selected code in a Neovim buffer.
-				- Generating unit tests for the selected code.
-				- Proposing fixes for problems in the selected code.
-				- Scaffolding code for a new workspace.
-				- Finding relevant code to the user's query.
-				- Proposing fixes for test failures.
-				- Answering questions about Neovim.
-				- Running tools.
+			-- 	Your core tasks include:
+			-- 	- Answering general programming questions.
+			-- 	- Explaining how the code in a Neovim buffer works.
+			-- 	- Reviewing the selected code in a Neovim buffer.
+			-- 	- Generating unit tests for the selected code.
+			-- 	- Proposing fixes for problems in the selected code.
+			-- 	- Scaffolding code for a new workspace.
+			-- 	- Finding relevant code to the user's query.
+			-- 	- Proposing fixes for test failures.
+			-- 	- Answering questions about Neovim.
+			-- 	- Running tools.
 
-				You must:
-				- Follow the user's requirements carefully and to the letter.
-				- Keep your answers short and impersonal, especially if the user responds with context outside of your tasks.
-				- Minimize other prose.
-				- Use Markdown formatting in your answers.
-				- Include the programming language name at the start of the Markdown code blocks.
-				- Avoid including line numbers in code blocks.
-				- Avoid wrapping the whole response in triple backticks.
-				- Only return code that's relevant to the task at hand. You may not need to return all of the code that the user has shared.
-				- Use actual line breaks instead of '\n' in your response to begin new lines.
-				- Use '\n' only when you want a literal backslash followed by a character 'n'.
-				- All non-code responses must be in %s.
+			-- 	You must:
+			-- 	- Follow the user's requirements carefully and to the letter.
+			-- 	- Keep your answers short and impersonal, especially if the user responds with context outside of your tasks.
+			-- 	- Minimize other prose.
+			-- 	- Use Markdown formatting in your answers.
+			-- 	- Include the programming language name at the start of the Markdown code blocks.
+			-- 	- Avoid including line numbers in code blocks.
+			-- 	- Avoid wrapping the whole response in triple backticks.
+			-- 	- Only return code that's relevant to the task at hand. You may not need to return all of the code that the user has shared.
+			-- 	- Use actual line breaks instead of '\n' in your response to begin new lines.
+			-- 	- Use '\n' only when you want a literal backslash followed by a character 'n'.
+			-- 	- All non-code responses must be in %s.
 
-				When given a task:
-				1. Think step-by-step and describe your plan for what to build in pseudocode, written out in great detail, unless asked not to do so.
-				2. Output the code in a single code block, being careful to only return relevant code.
-				3. You should always generate short suggestions for the next user turns that are relevant to the conversation.
-				4. You can only give one reply for each conversation turn.]]
-			end,
+			-- 	When given a task:
+			-- 	1. Think step-by-step and describe your plan for what to build in pseudocode, written out in great detail, unless asked not to do so.
+			-- 	2. Output the code in a single code block, being careful to only return relevant code. Do NOT place the code in a code block, i.e., don't include backticks.
+			-- 	3. You should always generate short suggestions for the next user turns that are relevant to the conversation.
+			-- 	4. You can only give one reply for each conversation turn.]]
+			-- end,
 			adapters = {
+				qwen2_5_coder = function()
+					return require("codecompanion.adapters").extend("ollama", {
+						name = "qwen2.5-coder:7b",
+						env = {
+							url = "http://127.0.0.1:11434",
+						},
+						headers = {
+							["Content-Type"] = "application/json",
+						},
+						parameters = {
+							sync = true,
+						},
+						schema = {
+							model = {
+								default = "qwen2.5-coder:7b",
+							},
+						},
+					})
+				end,
+				deepseek_r1_8b = function()
+					return require("codecompanion.adapters").extend("ollama", {
+						name = "deepseek-r1:8b",
+						env = {
+							url = "http://127.0.0.1:11434",
+						},
+						headers = {
+							["Content-Type"] = "application/json",
+						},
+						parameters = {
+							sync = true,
+						},
+						schema = {
+							model = {
+								default = "deepseek-r1:8b",
+							},
+						},
+					})
+				end,
 				deepseek_r1_14b = function()
 					return require("codecompanion.adapters").extend("ollama", {
 						name = "deepseek-r1:14b",
@@ -107,23 +145,14 @@ return {
 					return require("codecompanion.adapters").extend("ollama", {
 						name = "llama3.1",
 						env = {
-							url = "http://100.91.137.78:11435",
+							url = "http://127.0.0.1:11434",
 						},
 						headers = {
 							["Content-Type"] = "application/json",
 						},
-						parameters = {
-							sync = true,
-						},
 						schema = {
 							model = {
 								default = "llama3.1",
-							},
-							num_ctx = {
-								default = 16384,
-							},
-							num_predict = {
-								default = -1,
 							},
 						},
 					})
@@ -156,10 +185,10 @@ return {
 			},
 			strategies = {
 				chat = {
-					adapter = "deepseek_r1_14b",
+					adapter = "llama3_1",
 				},
 				inline = {
-					adapter = "codestral",
+					adapter = "qwen2_5_coder",
 				},
 			},
 		})

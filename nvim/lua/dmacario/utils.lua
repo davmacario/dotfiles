28a3eb8d069c0eb978cbc70cd3c04930f767bc4d
Davide Macario <dav.macario@gmail.com>
2025-03-19 22:10:09 +0100

    feat: create function to truncate lualine component length

--- nvim/lua/dmacario/utils.lua
+++ nvim/lua/dmacario/utils.lua
@@ -265,6 +265,24 @@ function M.breadcrumbs()
 	return navic.format_data({ prefix_data })
 end
 
+--- Function to be passed to Lualine component (fmt) to truncate component
+--- @param trunc_width number|nil trunctates component when screen width is less then trunc_width
+--- @param trunc_len number truncates component to trunc_len number of chars
+--- @param hide_width number|nil hides component when window width is smaller then hide_width
+--- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+--- return function that can format the component accordingly
+function M.trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+	return function(str)
+		local win_width = vim.fn.winwidth(0)
+		if hide_width and win_width < hide_width then
+			return ""
+		elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+			return str:sub(1, trunc_len) .. (no_ellipsis and "" or "...")
+		end
+		return str
+	end
+end
+
 -- Get dir of current file
 function M.get_current_file_directory()
 	local source = debug.getinfo(1, "S").source

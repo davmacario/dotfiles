86987e20d39e78e9f663db43cbe250d111b9beb2
Davide Macario <dav.macario@gmail.com>
2025-04-02 19:48:31 +0200

    chore: add log

--- setup.sh
+++ setup.sh
@@ -132,6 +132,7 @@ if [[ "$SHELL" != *zsh* ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
     # Install powerlevel10k
+    log "Installing p10k"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-"$HOME"/.oh-my-zsh/custom}/themes/powerlevel10k"
 fi
 

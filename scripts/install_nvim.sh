2fe49879f25eb26afa5998842393381692bfc3f3
Davide Macario <dav.macario@gmail.com>
2025-04-18 12:31:36 +0200

    fix: pass actual home to nvim install script
    (cherry picked from commit 876522c3766cc5634e027d1744da5aa2c8a4bd77)

--- scripts/install_nvim.sh
+++ scripts/install_nvim.sh
@@ -3,8 +3,8 @@
 # Nvim always compiled from source to avoid undesired breaking changes
 # at 8 in the morning after a `brew update`
 VERSION="${1:-v0.10.4}"
-GHDIR="$HOME/github"
-mkdir "$GHDIR/neovim"
+GHDIR="${3:-$HOME}/github"
+mkdir -p "$GHDIR/neovim"
 git clone -b "$VERSION" https://github.com/neovim/neovim.git "$GHDIR/neovim/neovim"
 pushd "$GHDIR/neovim/neovim" || exit 1
 make CMAKE_BUILD_TYPE="${NVIM_CMAKE_BUILD_TYPE:-"RelWithDebInfo"}"

4d0e4cb427031fee7980621bb01d33425b708e5a
Davide Macario <dav.macario@gmail.com>
2025-05-01 14:50:18 +0200

    fix: use visual whitespace version compatible with nvim 0.10

--- nvim/lua/dmacario/lazy/visual-whitespace.lua
+++ nvim/lua/dmacario/lazy/visual-whitespace.lua
@@ -1,13 +1,14 @@
 return {
-  "mcauley-penney/visual-whitespace.nvim",
-  config = function()
-    require("visual-whitespace").setup({
-      highlight = { fg = "#908070", bg = "#665c54" },
-      space_char = "·",
-      tab_char = "→",
-      nl_char = "↲",
-      cr_char = "←",
-      enabled = true,
-    })
-  end,
+	"mcauley-penney/visual-whitespace.nvim",
+	commit = "compat-v10",
+	config = function()
+		require("visual-whitespace").setup({
+			highlight = { fg = "#908070", bg = "#665c54" },
+			space_char = "·",
+			tab_char = "→",
+			nl_char = "↲",
+			cr_char = "←",
+			enabled = true,
+		})
+	end,
 }

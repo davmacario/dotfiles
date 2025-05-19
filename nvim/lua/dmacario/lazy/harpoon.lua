504ce8f79f04ab11903c9b68dd5a96bc3d0d713f
Davide Macario <dav.macario@gmail.com>
2025-05-19 13:06:16 +0200

    feat: change harpoon keybind
    prevent conflict with commands to center viewport
    
    (cherry picked from commit afd9c99503fa72fbbd7b63da1b007ab29f1ea7bf)

--- nvim/lua/dmacario/lazy/harpoon.lua
+++ nvim/lua/dmacario/lazy/harpoon.lua
@@ -5,7 +5,7 @@ return {
 		local ui = require("harpoon.ui")
 
 		vim.keymap.set("n", "<leader>a", mark.add_file)
-		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { noremap = true })
+		vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, { noremap = true })
 
 		vim.keymap.set("n", "<C-h>", function()
 			ui.nav_file(1)

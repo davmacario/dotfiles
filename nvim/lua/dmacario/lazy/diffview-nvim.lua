05084ee3aefc05f1a8c727fce558b8b903b7e95f
Davide Macario <dav.macario@gmail.com>
2025-07-07 10:27:39 +0200

    chore: disable line wrap when using diffview

--- nvim/lua/dmacario/lazy/diffview-nvim.lua
+++ nvim/lua/dmacario/lazy/diffview-nvim.lua
@@ -1,3 +1,12 @@
 return {
 	"sindrets/diffview.nvim",
+	config = function()
+		require("diffview").setup({
+			hooks = {
+				diff_buf_read = function(bufnr, ctx)
+					vim.opt_local.wrap = false
+				end,
+			},
+		})
+	end,
 }

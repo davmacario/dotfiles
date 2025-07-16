344a9f9bdbdd414fb6b13c98d79a505b0051990d
Davide Macario <dav.macario@gmail.com>
2025-07-16 23:08:00 +0200

    chore: bump up nvim default version
    (cherry picked from commit 3a6b05f3ac9c0954ee8bc6d812bc7194e2aefc09)

--- scripts/install_nvim.sh
+++ scripts/install_nvim.sh
@@ -2,12 +2,18 @@
 
 # Nvim always compiled from source to avoid undesired breaking changes
 # at 8 in the morning after a `brew update`
-VERSION="${1:-v0.10.4}"
+VERSION="${1:-v0.11.3}"
 GHDIR="${3:-$HOME}/github"
 mkdir -p "$GHDIR/neovim"
-git clone -b "$VERSION" https://github.com/neovim/neovim.git "$GHDIR/neovim/neovim"
+if [ ! -d "$GHDIR/neovim/neovim" ]; then
+    git clone -b "$VERSION" https://github.com/neovim/neovim.git "$GHDIR/neovim/neovim"
+else
+    echo "Found local clone of Neovim repo"
+fi
 pushd "$GHDIR/neovim/neovim" || exit 1
 make CMAKE_BUILD_TYPE="${NVIM_CMAKE_BUILD_TYPE:-"RelWithDebInfo"}"
 make install
 popd || exit 1
-chown -R "$2":"$2" "$GHDIR/neovim"
+if [ -n "$2" ]; then
+    chown -R "$2":"$2" "$GHDIR/neovim"
+fi

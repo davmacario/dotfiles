90406dbc4a93e884007e51b590e0e4447d4551c1
Davide Macario <dav.macario@gmail.com>
2026-02-25 00:57:19 +0100

    chore: clone needed deps
    (cherry picked from commit 62d53d864dcdc17c968ca4879a067b158af50da0)

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -25,8 +25,11 @@ correct_ownership() {
 install_plugins() {
     git clone "https://github.com/zsh-users/zsh-autosuggestions" "${ZSH_CUSTOM:-$actual_home/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
     git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" "${ZSH_CUSTOM:-$actual_home/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+    git clone "https://github.com/jeffreytse/zsh-vi-mode.git" "${ZSH_CUSTOM:-$actual_home/.oh-my-zsh/custom}/plugins/zsh-vi-mode"
+
     correct_ownership "${ZSH_CUSTOM:-$actual_home/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
     correct_ownership "${ZSH_CUSTOM:-$actual_home/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+    correct_ownership "${ZSH_CUSTOM:-$actual_home/.oh-my-zsh/custom}/plugins/zsh-vi-mode"
 }
 
 # Install ZSH

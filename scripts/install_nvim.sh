32913f38a88ac374912f9d8cbba2bd904dfd34e6
Davide Macario <dav.macario@gmail.com>
2026-04-16 01:25:32 +0200

    feat: allow to download neovim from releases

--- scripts/install_nvim.sh
+++ scripts/install_nvim.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 
-# Nvim always compiled from source to avoid undesired breaking changes
-# at 8 in the morning after a `brew update`
-VERSION="${1:-v0.11.3}"
-GHDIR=${GHDIR:-"$HOME/github"}
-mkdir -p "$GHDIR/neovim"
-if [ ! -d "$GHDIR/neovim/neovim" ]; then
-    git clone -b "$VERSION" https://github.com/neovim/neovim.git "$GHDIR/neovim/neovim"
+NVIM_VERSION="${NVIM_VERSION:-v0.11.3}"
+COMPILE="false"
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --nvim-version)
+            NVIM_VERSION="$2"
+            shift 2
+            ;;
+        --compile)
+            COMPILE="true"
+            shift
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$COMPILE" = "true" ]; then
+    echo "Compiling Nvim $NVIM_VERSION from source"
+    GHDIR=${GHDIR:-"$HOME/github"}
+    mkdir -p "$GHDIR/neovim"
+    if [ ! -d "$GHDIR/neovim/neovim" ]; then
+        git clone -b "$NVIM_VERSION" https://github.com/neovim/neovim.git "$GHDIR/neovim/neovim"
+    else
+        echo "Found local clone of Neovim repo"
+    fi
+    pushd "$GHDIR/neovim/neovim" || exit 1
+    git pull -p --tags
+    make CMAKE_BUILD_TYPE="${NVIM_CMAKE_BUILD_TYPE:-"RelWithDebInfo"}"
+    sudo make install
+    popd || exit 1
 else
-    echo "Found local clone of Neovim repo"
+    echo "Downloading Nvim $NVIM_VERSION from releases"
+    curl -LO "https://github.com/neovim/neovim/releases/$NVIM_VERSION/download/nvim-linux-x86_64.tar.gz"
+    sudo rm -rf /opt/nvim-linux-x86_64
+    sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+    ln -s /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+    rm nvim-linux-x86_64.tar.gz
 fi
-pushd "$GHDIR/neovim/neovim" || exit 1
-git pull -p --tags
-make CMAKE_BUILD_TYPE="${NVIM_CMAKE_BUILD_TYPE:-"RelWithDebInfo"}"
-sudo make install
-popd || exit 1
